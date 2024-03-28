a754ed5c61996b0e64340994f01feb5dd054cfa8
efe <121581223+efetekin5@users.noreply.github.com>
2024-03-28 13:57:41 +0300

    link github repo for people to find secret code for becoming an admin

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 The secret code for joining the club: members only
+The secret code for becoming an admin: admin
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,34 +1,36 @@
-const asyncHandler = require("express-async-handler");
-const { body, validationResult } = require("express-validator");
-const User = require("../models/user");
+const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+const User = require('../models/user');
 
 exports.newAdminGet = (req, res, next) => {
-  res.render("adminForm");
+  res.render('adminForm', {
+    title: 'Enter secret code to become an admin'
+  });
 };
 
 exports.newAdminPost = [
-  body("adminCode").notEmpty(),
+  body('adminCode').notEmpty(),
 
   asyncHandler(async (req, res, next) => {
     const error = validationResult(req);
 
     if (!error.isEmpty()) {
-      res.render("adminForm", {
-        errorMessage: "Invalid Code",
+      res.render('adminForm', {
+        title: 'Invalid Code',
       });
     } else {
-        if(req.body.adminCode === 'admin') {
-            const currentUser = await User.findOne({ _id: req.user.id });
-            currentUser.admin = true;
-            currentUser.membershipStatus = true;
-    
-            currentUser.save();
-            res.redirect('/');
-        } else {
-            res.render('adminForm', {
-                errorMessage: 'Invalid Code',
-            })
-        }
+      if (req.body.adminCode === 'admin') {
+        const currentUser = await User.findOne({ _id: req.user.id });
+        currentUser.admin = true;
+        currentUser.membershipStatus = true;
+
+        currentUser.save();
+        res.redirect('/');
+      } else {
+        res.render('adminForm', {
+          title: 'Invalid Code',
+        });
+      }
     }
   }),
 ];
